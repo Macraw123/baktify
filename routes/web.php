1505620d2e1646a6ab1b4aca3df62d7168043cc3
JK19-1 <jsentosa88@yahoo.com>
2022-05-26 14:36:21 +0700

    View Required

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CheckoutController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +20,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', function () {
+    return view('about')->with('user', Auth::user());
+})->name('about');
+
+Route::get('/products', [ProductController::class, 'index'])->name('products');
+Route::get('/add-product', [ProductController::class, 'addProductView'])->middleware(['admin']);
+Route::get('/update-product/{id}', [ProductController::class, 'updateProductView'])->middleware(['admin']);
+Route::post('/product', [ProductController::class, 'create']);
+Route::put('/product', [ProductController::class, 'update']);
+Route::delete('/product', [ProductController::class, 'destroy']);
+Route::get('/products/search', [ProductController::class, 'search']);
+
+Route::get('/products/{id}', [ProductController::class, 'detail']);
+Route::post('/add-cart', [CartController::class, 'addCart']);
+
+Route::get('/cart', [CartController::class, 'index'])->middleware(['member']);
+Route::put('/cart', [CartController::class, 'updateCart'])->middleware(['member']);
+
+Route::get('/categories', [CategoryController::class, 'index'])->middleware(['admin'])->name('categories');
+Route::post('/categories', [CategoryController::class, 'create'])->middleware(['admin']);
+
+Route::get('/profile', [UserController::class, 'index'])->middleware(['auth']);
+Route::get('/profile/detail', [UserController::class, 'detail'])->middleware(['auth']);
+Route::put('/profile', [UserController::class, 'update'])->middleware(['auth']);
+
+Route::get('/checkout', [CheckoutController::class, 'index'])->middleware(['member']);
+Route::post('/checkout', [CheckoutController::class, 'store'])->middleware(['member']);
+Route::get('/transactions', [CheckoutController::class, 'viewTransactions'])->middleware(['member']);
+
+require __DIR__ . '/auth.php';
